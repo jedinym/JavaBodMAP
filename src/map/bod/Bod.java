4be54fc6285ba0b8df06167be00f70df2b3414f0
Martin Jediny <jedinym@gmail.com>
2020-03-05 10:06:09 +0100

    first commit

---
+++ src/map/bod/Bod.java
@@ -0,0 +1,12 @@
+package map.bod;
+
+public interface Bod
+{
+    float getX();
+    float getY();
+    float[] getXY();
+    void setX(float _x);
+    void setY(float _y);
+    void setXY(float[] xy);
+
+}
